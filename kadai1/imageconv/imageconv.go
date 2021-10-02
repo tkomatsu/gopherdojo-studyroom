88026bfd51b0289d0c8e21a0f9959dd205b1574e
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-10-02 10:03:21 +0900

    refactor(kadai1):

--- kadai1/imageconv/imageconv.go
+++ kadai1/imageconv/imageconv.go
@@ -9,44 +9,34 @@ import (
 	_ "image/jpeg"
 	"image/png"
 	_ "io"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
-func Convert(dir string, srcType string) {
-	files, err := ioutil.ReadDir(dir)
+func Convert(filename string) error {
+	img, err := readImage(filename)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v: %v\n", dir, err)
-		os.Exit(1)
+		return fmt.Errorf("%s", err)
 	}
-	for _, file := range files {
-		fmt.Println(file.Name())
-		if file.IsDir() {
-			fmt.Println()
-			Convert(filepath.Join(dir, file.Name()), srcType)
-		} else {
-			if strings.HasSuffix(file.Name(), ".jpg") {
-				jpg2png(filepath.Join(dir, file.Name()))
-			}
-		}
-	}
+	return writeImage(img, filename)
 }
 
-func jpg2png(filename string) {
-	file, err := os.Open(filename)
+func readImage(filepath string) (image.Image, error) {
+	file, err := os.Open(filepath)
 	if err != nil { log.Print(err) }
 	defer file.Close()
 
-	img, format, err := image.Decode(file)
-	if err != nil { log.Print(err) }
-	fmt.Fprintf(os.Stderr, "format: %v", format)
+	img, _, err := image.Decode(file)
+	return img, err
+}
 
-	outfilename := strings.TrimSuffix(filename, ".jpg") + ".png"
-
+func writeImage(src image.Image, filepath string) error {
+	outfilename := strings.TrimSuffix(filepath, ".jpg") + ".png"
 	out, err := os.Create(outfilename)
-
-	png.Encode(out, img)
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+	png.Encode(out, src)
+	return nil
 }
--- kadai1/main.go
+++ kadai1/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"flag"
+	"io/ioutil"
+	"path/filepath"
+	"os"
+	"strings"
 
 	"github.com/tkomatsu/gopherdojo-studyroom/kadai1/imageconv"
 )
@@ -12,6 +17,26 @@ func main() {
 	flag.Parse()
 	paths := flag.Args()
 	for _, path := range paths {
-		imageconv.Convert(path, *srcType)
+		doRecursive(path, *srcType)
+	}
+}
+
+func doRecursive(dir string, srcType string) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v: %v\n", dir, err)
+		os.Exit(1)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			doRecursive(filepath.Join(dir, file.Name()), srcType)
+		} else {
+			if strings.HasSuffix(file.Name(), ".jpg") {
+				err := imageconv.Convert(filepath.Join(dir, file.Name()))
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+			}
+		}
 	}
 }
